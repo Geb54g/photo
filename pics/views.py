89018008a9054cfd2474062542b998fe6dbfc445
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-30 12:21:29 +0300

    ft:rendered all the tamplates

--- pics/views.py
+++ pics/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def gallery(request):
+    return render (request,'pics/gallery.html')
+
+
+def viewpics(request):
+    return render (request,'pics/pics.html')
+
+
+def addpics(request):
+    return render (request,'pics/add.html')
