97cf1df941ba399b5d26364b2f55c96d7997246a
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-30 15:51:51 +0300

    st:made migration for the database

--- pics/models.py
+++ pics/models.py
@@ -3,3 +3,15 @@ from django.db import models
 # Create your models here.
 class category(models.Model):
     name = models.CharField(max_length=150,null=False, blank= False)
+    
+    def __str__(self):
+        return self.name
+
+
+class photo(models.Model):
+    category = models.ForeignKey(category, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(null = False, blank =False)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.description
